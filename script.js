8128a9d9ead2f765fe083c058ce0710760be7f1f
Kaleek03 <Kadec208@gmail.com>
2023-01-15 12:14:31 -0700

    Set new var to getcomputer to get static answer

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 
 
- function versus(playerSelection ){
-    if (getComputerChoice() == "Rock" && playerSelection == "Rock" ) {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+function versus(playerSelection ){
+    let computerChoice = getcomputerChoice();
+
+    if (computerChoice == "Rock" && playerSelection == "Rock" ) {
+        return `You chose ${playerSelection } I chose ${computerChoice } so Tie game`
     } 
-    else if (getComputerChoice() == "Paper" && playerSelection == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+    else if (computerChoice == "Paper" && playerSelection == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so Tie game`
     } 
-    else if (getComputerChoice() == "Scissors" && playerSelection == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+    else if (computerChoice == "Scissors" && playerSelection == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so Tie game`
     } 
-    else if(getComputerChoice() == "Rock" && playerSelection == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(computerChoice == "Rock" && playerSelection == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
-    else if(getComputerChoice() == "Rock" && playerSelection == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!!`
+    else if(computerChoice == "Rock" && playerSelection == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!!`
     } 
-    else if(getComputerChoice() == "Paper" && playerSelection == "Rock") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(computerChoice == "Paper" && playerSelection == "Rock") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
-    else if(getComputerChoice() == "Paper" && playerSelection == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!!`
+    else if(computerChoice == "Paper" && playerSelection == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!!`
     } 
-    else if(getComputerChoice() == "Scissors" && playerSelection == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(computerChoice == "Scissors" && playerSelection == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
-    else if(getComputerChoice() == "Scissors" && playerSelection == "Rock") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!`
+    else if(computerChoice == "Scissors" && playerSelection == "Rock") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!`
     } 
-    else if(playerSelection == "Rock" && getComputerChoice() == "Rock") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+    else if(playerSelection == "Rock" && computerChoice == "Rock") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so Tie game`
     } 
-    else if (playerSelection == "Paper" && getComputerChoice() == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+    else if (playerSelection == "Paper" && computerChoice == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so Tie game`
     } 
-    else if (playerSelection == "Scissors" && getComputerChoice() == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so You chose ${playerSelection } I chose ${getComputerChoice() } so Tie game`
+    else if (playerSelection == "Scissors" && computerChoice == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so You chose ${playerSelection } I chose ${computerChoice() } so Tie game`
     } 
-    else if(playerSelection == "Rock" && getComputerChoice() == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!!`
+    else if(playerSelection == "Rock" && computerChoice == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!!`
     } 
-    else if(playerSelection == "Rock" && getComputerChoice() == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(playerSelection == "Rock" && computerChoice == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
-    else if(playerSelection == "Paper" && getComputerChoice() == "Rock") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!!`
+    else if(playerSelection == "Paper" && computerChoice == "Rock") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!!`
     } 
-    else if(playerSelection == "Paper" && getComputerChoice() == "Scissors") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(playerSelection == "Paper" && computerChoice == "Scissors") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
-    else if(playerSelection == "Scissors" && getComputerChoice() == "Paper") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I lost! and you WON!!`
+    else if(playerSelection == "Scissors" && computerChoice == "Paper") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I lost! and you WON!!`
     } 
-    else if(playerSelection == "Scissors" && getComputerChoice() == "Rock") {
-        return `You chose ${playerSelection } I chose ${getComputerChoice() } so I won!! and you LOST!`
+    else if(playerSelection == "Scissors" && computerChoice == "Rock") {
+        return `You chose ${playerSelection } I chose ${computerChoice } so I won!! and you LOST!`
     } 
     else {
         console.log("ERROR")
@@ -61,7 +63,7 @@
  }
 
 
- function getComputerChoice() {
+ function getcomputerChoice() {
     const gameArray = ["Rock", "Paper", "Scissors"]
     const randomizer = gameArray[(Math.random() * gameArray.length) | 0];
     return randomizer
